e37ad66fcd1d15144259bbf6fcba5437bffbdc99
Emilia Kirschenbaum <ekubokirs@gmail.com>
2013-11-05 10:09:51 -0800

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Emilia Kirschenbaum on 11/5/13.
+//  Copyright (c) 2013 BedTimeTaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
