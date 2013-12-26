42750fb90ec76a9ecfb7853a19f29293b0baab9c
Emilia Kirschenbaum <ekubokirs@gmail.com>
2013-12-26 08:27:12 -0800

    current work on project

---
+++ Ribbit/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  Ribbit
+//
+//  Created by Emilia Kirschenbaum on 12/5/13.
+//  Copyright (c) 2013 BedTimeTaco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UITableViewController
+
+@end
---
+++ Ribbit/InboxViewController.m
@@ -0,0 +1,49 @@
+//
+//  InboxViewController.m
+//  Ribbit
+//
+//  Created by Emilia Kirschenbaum on 12/5/13.
+//  Copyright (c) 2013 BedTimeTaco. All rights reserved.
+//
+
+#import "InboxViewController.h"
+
+@interface InboxViewController ()
+
+@end
+
+@implementation InboxViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self performSegueWithIdentifier:@"showLogin" sender:self];
+    
+}
+
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+@end
